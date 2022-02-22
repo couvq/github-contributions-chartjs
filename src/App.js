83fe0c2485390d7ba622be0ab75c79ef65e176a7
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-22 18:00:20 -0500

    recreated App.js - need to use chartjs as learning that is the point of this project. Setup a clean slate for development tomorrow.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core";
-import { Card, Grid } from "@material-ui/core";
-import {
-  ArgumentAxis,
-  ValueAxis,
-  Chart,
-  LineSeries,
-} from "@devexpress/dx-react-chart-material-ui";
-
-import SectionHeader from "../src/components/SectionHeader";
-
-const useStyles = makeStyles((theme) => ({
-  chartCard: {
-    padding: "40px",
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-}));
+import React from 'react';
+import SectionHeader from './components/SectionHeader';
 
 const App = () => {
-  const classes = useStyles();
-  // Sample data
-  const data = [
-    { x: 1, y: 27 },
-    { x: 15, y: 56 },
-    { x: 30, y: 129 }
-  ];
+
+
   return (
     <>
-      <SectionHeader />
-
-      <Card className={classes.chartCard} >
-        <Chart data={data}>
-          <ArgumentAxis />
-          <ValueAxis />
-
-          <LineSeries valueField="y" argumentField="x" />
-        </Chart>
-      </Card>
+        <SectionHeader />
     </>
   );
-};
+}
 
 export default App;
